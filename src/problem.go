70e789dc7bd8fd1e42b31c1f20fdc05063ea5247
Raphael Bosshard <raphael.bosshard@gmail.com>
2011-12-14 01:10:02 +0100

    refine state, add error return value to Solve

--- src/problem.go
+++ src/problem.go
@@ -1,6 +1,8 @@
-package "problem"
+package planning
 
-import "state"
+type NoSolutionError struct {
+  message string
+}
 
 type Action interface {
   IsAllowed(a State) bool //asks whether the given action is possible this state
@@ -9,11 +11,14 @@ type Action interface {
 
 type Problem interface {
   Child(parent State, action Action) (child Node)
-  Inital() State
-  Goal() State
+  Inital() (initial State)
+  Goal() (goal State)
+  IsGoal(state State) (isGoal bool)
+  StepCost(state State) (stepCost float32)
+  Actions(state State) (actions []Action)
 }
 
 type Solver interface {
-  Solve(problem Problem) (solution Node) 
+  Solve(problem Problem) (solution Node, error NoSolutionError)
 }
 
