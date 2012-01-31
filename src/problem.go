4d9bcfd2ce2990726d9847b9517a52dc6b718c48
Raphael Bosshard <raphael.bosshard@gmail.com>
2012-01-31 13:56:12 +0100

    Some more structs and functions

--- src/problem.go
+++ src/problem.go
@@ -4,21 +4,65 @@ type NoSolutionError struct {
   message string
 }
 
-type Action interface {
-  IsAllowed(a State) bool //asks whether the given action is possible this state
-  Do(a State) State //execute action and return new state
+type Action interface {}
+
+type State interface {
+  Hashable
+}
+
+type Node struct {
+  cost float32
+  state State
+  action Action
+  parent *Node
+}
+
+type Error struct {
+  message string
+}
+
+func NewNode(parent *Node, action Action, state State) (n *Node) {
+  n = new(Node)
+  n.state = state
+  n.action = action
+  n.parent = parent
+  return
+}
+
+func (n Node) Equals (other Hashable) bool {
+  o := other.(*Node)
+  if n.state.Equals(o.state) {
+    return true
+  }
+  return false
+}
+
+func (n Node) Hash() string {
+  return n.state.Hash()
+}
+
+func (n Node) Compare (other Comparable) int {
+  o := other.(*Node)
+
+  if n.cost < o.cost {
+    return -1
+  } else if n.cost == o.cost {
+    return 0
+  } else if n.cost > o.cost {
+    return 1
+  }
+  panic("there is something seriously wrong..")
 }
 
 type Problem interface {
-  Child(parent State, action Action) (child Node)
-  Inital() (initial State)
+  Initial() (initial State)
   Goal() (goal State)
-  IsGoal(state State) (isGoal bool)
-  StepCost(state State) (stepCost float32)
-  Actions(state State) (actions []Action)
+  IsGoal(state State) bool
+  StepCost(cost float32, origin State, action Action, destination State) (stepCost float32)
+  Successors(state State) (successors map[string]State)
 }
 
 type Solver interface {
-  Solve(problem Problem) (solution Node, error NoSolutionError)
+  Solve(problem Problem) (solution interface{}, error NoSolutionError)
 }
 
