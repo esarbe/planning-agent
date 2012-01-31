675e386e60287306afda3069b4dbb00fad9bc93b
Raphael Bosshard <raphael.bosshard@gmail.com>
2012-01-31 14:09:02 +0100

    unit test for hanoi

---
+++ src/hanoi_test.go
@@ -0,0 +1,13 @@
+package planning
+
+import "testing"
+import "fmt"
+
+func TestNewHanoi(t *testing.T) {
+  tower := &TowerOfHanoi{3, HanoiState{0,0,0,0}, HanoiState{2,2,2,2}}
+  fmt.Println("built new tower")
+
+  successors := tower.Successors(HanoiState{0,0,0,0})
+
+  fmt.Println("Successors:", successors)
+}
