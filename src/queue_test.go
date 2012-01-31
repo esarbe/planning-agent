1dfbaa69b56a76305a7082ce0793a62666b788c8
Raphael Bosshard <raphael.bosshard@gmail.com>
2012-01-31 13:56:31 +0100

    fixed tests

--- src/queue_test.go
+++ src/queue_test.go
@@ -3,14 +3,32 @@ package planning
 import "testing"
 import "fmt"
 
+type intState int
 
-func TestPush(t *testing.T) {
-  fmt.Println("testing queue push...")
-  q := &PriorityQueue{}
+func (s intState) Equals (o Hashable) bool {
+  return s == o.(intState)
+}
+
+func (s intState) Hash() string {
+  return fmt.Sprintln(s)
+}
+
+func (s intState) Compare (o Comparable) int {
+  return int(s - o.(intState))
+}
+
+func TestSlice(t *testing.T) {
+  fmt.Println("testing slice push")
+}
+
+func TestPriorityQueue(t *testing.T) {
+  fmt.Println("testing priority queue...")
+  q := NewPriorityQueue()
 
   for i :=0; i < 100; i+=1 {
-    n := NamedNode {nil, nil, float32(i), nil}
-    q.Push(&n)
+    n := NewNode(nil, nil, intState(i))
+    n.cost = float32(i)
+    q.Push(n)
   }
 
   if q.Len() != 100 {
@@ -21,42 +39,12 @@ func TestPush(t *testing.T) {
 
   fmt.Println("testing queue pop...")
   for i := 0; i < 50; i+=1 {
+
     q.Pop()
   }
 
   if q.Len() != 50 {
-    fmt.Println("queue does not have expected length")
-  }
-
-  fmt.Println("done.")
-}
-
-
-func TestSort(t *testing.T) {
-  fmt.Println("testing queue sort...")
-
-  n1 := NamedNode{nil, nil, float32(1), nil}
-  n2 := NamedNode{nil, nil, float32(2), nil}
-  n3 := NamedNode{nil, nil, float32(3), nil}
-  n4 := NamedNode{nil, nil, float32(4), nil}
-
-  q := PriorityQueue {&n1, &n2, &n3, &n4}
-
-  n := q.Pop()
-
-  if n.PathCost() != 1 {
-    fmt.Println("expected pathcost: 1, actual:", n.PathCost())
-    t.FailNow()
-  }
-
-  n4.SetPathCost(float32(0.1))
-  q.Sort()
-
-  n = q.Pop()
-
-  if n.PathCost() != 0.1 {
-    fmt.Println("expected pathcost: 0.1 , actual:", n.PathCost())
-    t.FailNow()
+    fmt.Println("queue does not have expected length of 50, is ", q.Len()," instead")
   }
 
   fmt.Println("done.")
