aa5ad7a675979712b3b6a71a155079aec97aaa75
Raphael Bosshard <raphael.bosshard@gmail.com>
2012-03-04 14:35:28 +0100

    first sketch of DataManager

--- src/loader/jsonloader.go
+++ src/loader/jsonloader.go
@@ -1,1 +1,16 @@
 package loading
+
+import "reflect"
+
+type DataManager interface {
+  Index() []string
+  Get(identifier string) interface{}
+  Store(identifier, interface{}) bool
+}
+
+type JSonDataManager struct {
+  location string
+  kind reflect.Kind
+
+}
+
