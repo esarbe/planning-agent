a62c0ab45acd76e23ceda94bbd156b47c15328df
Raphael Bosshard <raphael.bosshard@gmail.com>
2012-03-04 14:35:09 +0100

    formatting

--- src/collection/priority_queue.go
+++ src/collection/priority_queue.go
@@ -3,24 +3,24 @@ package collection
 import "sort"
 
 type PriorityQueue struct {
-	HashQueue
+  HashQueue
   compare Comparison
 }
 
 func NewPriorityQueue(c Comparison) (queue *PriorityQueue) {
-	queue = new(PriorityQueue)
+  queue = new(PriorityQueue)
   queue.compare = c
-	queue.HashQueue = *NewHashQueue()
-	return
+  queue.HashQueue = *NewHashQueue()
+  return
 }
 
 func (q *PriorityQueue) Push(c Comparable) {
-	q.HashQueue.Push(c)
-	q.Sort()
+  q.HashQueue.Push(c)
+  q.Sort()
 }
 
 func (q *PriorityQueue) Sort() {
-	sort.Sort(q)
+  sort.Sort(q)
 }
 
 func (q *PriorityQueue) Less(i, j int) bool {
