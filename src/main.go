ee485f629717b4befcaa88985b4786e926c29ae9
Raphael Bosshard <raphael.bosshard@gmail.com>
2012-03-07 17:19:57 +0100

    extend cli functions

--- src/main.go
+++ src/main.go
@@ -13,41 +13,7 @@ type Map map[string]interface{}
 func main() {
   run := true
 
-  mc := cmd.NewMetaCommand()
-  index := func (arguments []string) (string, bool) {
-    fmt.Println(mc.Index())
-    return "", true
-  }
-
-  quit := func (arguments []string) (string, bool) {
-    run = false;
-    return "quitting", true
-  }
-
-  mc.RegisterFunction("index", index)
-  mc.RegisterFunction("quit", quit)
-
-  loop := mainloop.NewConsoleMainloop()
-
-  loop.RunFunction = func() bool {
-    data, _, error := loop.ReadLine()
-
-    if error != nil {
-      fmt.Println("error:", error)
-    }
-
-    input := strings.TrimSpace(string(data))
-    result, _ := mc.Do([]string{input})
-    loop.WriteString(result)
-    loop.Flush()
-
-    return run
-  }
-
-  mainloop.Loop(loop)
-
   fileLocation := "/home/raphael/source/planning-agent/data/test-state1.json"
-
   var problem HashPlanningProblem
 
   file, error := os.Open(fileLocation)
@@ -63,21 +29,83 @@ func main() {
     panic("")
   }
 
-  fmt.Println(problem.Init)
-  is := NewBooleanPredicate("is", "x", "y")
-  has := NewBooleanPredicate("has", "x", "z")
-  isAndHas := And(is, has)
-
-  params0 := map[string]string{"x": "barrel", "y": "round", "z": "rim"}
-  params1 := map[string]string{"x": "bar", "y": "steel" }
   init := NewHashKnowlegebase()
   init.SetSubjectFactory(problem.Init)
 
-  ok := isAndHas.Satisfied(init, params0)
-  fmt.Println("barrel is round and has rim", ok)
+  mc := cmd.NewMetaCommand()
 
-  ok = is.Satisfied(init, params1)
-  fmt.Println("bar is steel", ok)
+  index := func (arguments []string) (string, bool) {
+    fmt.Println(mc.Index())
+    return "", true
+  }
+
+  set := func (arguments []string) (string, bool) {
+    if len(arguments) < 3 {
+      return "not enough arguments, need: <subject> <predicate> <object>", false
+    }
+    predicate := NewBooleanPredicate(arguments[1], "x", "y")
+    predicate.State(init, map[string]string{"x": arguments[0], "y": arguments[2]})
+    return "", true
+  }
+
+  unset := func (arguments []string) (string, bool) {
+    if len(arguments) < 3 {
+      return "not enough arguments, need: <subject> <predicate> <object>", false
+    }
+    predicate := NewBooleanPredicate(arguments[1], "x", "y")
+    predicate.Retract(init, map[string]string{"x": arguments[0], "y": arguments[2]})
+    return "", true
+  }
+
+  query := func (arguments []string) (string, bool) {
+    if len(arguments) < 3 {
+      return "not enough arguments, need: <subject> <predicate> <object>", false
+    }
+    predicate := NewBooleanPredicate(arguments[1], "x", "y")
+    ok := predicate.Satisfied(init, map[string]string{"x": arguments[0], "y": arguments[2]})
+    return fmt.Sprintln(strings.Join(arguments, " "), ok), ok
+  }
+
+  show := func (arguments []string) (string, bool) {
+    if len(arguments) < 1 {
+      return "not enough arguments, need: <object>", false
+    }
+
+    subject, ok := init.Objects[arguments[0]]
+    return fmt.Sprint(subject), ok
+  }
+
+  quit := func (arguments []string) (string, bool) {
+    run = false;
+    return "quitting", true
+  }
+
+  mc.RegisterFunction("index", index)
+  mc.RegisterFunction("quit", quit)
+  mc.RegisterFunction("set", set)
+  mc.RegisterFunction("unset", unset)
+  mc.RegisterFunction("query", query)
+  mc.RegisterFunction("show", show)
+
+  loop := mainloop.NewConsoleMainloop(">")
+
+  loop.RunFunction = func() bool {
+    loop.Flush()
+    data, _, error := loop.ReadLine()
+
+    if error != nil {
+      fmt.Println("error:", error)
+    }
+
+    input := strings.TrimSpace(string(data))
+    result, _ := mc.Do(strings.Split(input, " "))
+    loop.WriteString(result)
+
+    return run
+  }
+
+  mainloop.Loop(loop)
+
 }
 
 
